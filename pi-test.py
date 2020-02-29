fe8863a839052b85adcb46d0f4ae764fc1f526ba
dzianismaroz <43498404+dzianismaroz@users.noreply.github.com>
2020-02-29 17:08:36 +0300

    Update pi-test.py

--- pi-test.py
+++ pi-test.py
@@ -4,11 +4,13 @@ import re
 import sys
 import logging
 import time
+import threading
 import RPi.GPIO as GPIO
 from subprocess import Popen
 from logging.handlers import RotatingFileHandler
 
-logger = logging.getLogger("Rotating Log")
+LOGGER = logging.getLogger("Rotating Log")
+
 
 #------------ CREATE LOG ROTATION CONFIG -----------------------
 def create_rotating_log(path):
@@ -19,75 +21,144 @@ def create_rotating_log(path):
     if not os.path.exists("logs/"):
         os.makedirs("logs/")
 
-    logger.setLevel(logging.DEBUG)
+    LOGGER.setLevel(logging.INFO)
 
     # add a rotating handler
     handler = RotatingFileHandler(path, maxBytes=1000*1024, backupCount=10, mode='a') # let max size be 1 Mb
     handler.setFormatter(logging.Formatter('%(asctime)s %(message)s', datefmt='%m-%d-%Y %H:%M:%S'))
-    logger.addHandler(handler)
+    LOGGER.addHandler(handler)
+
 
 create_rotating_log("logs/pi-test.log")
 
-R = re.compile("\d+")
+R = re.compile('\d+')
 
 pin_info = {}
 
-def read_base_path_config(file_name) :
+
+def log_exit():
+    LOGGER.error("###### ERROR raspin service being terminated")
+    sys.exit(-1)
+
+
+def read_base_path_config(file_name):
     try:
-        logger.debug("Opening configuration file [%s] ..." % file_name)
+        LOGGER.info("Opening configuration file [%s] ..." % file_name)
         return open(file_name.rstrip()).read()
-    except:
-        logger.error("Failed to read config file %s [ due to: %s" % (file_name, sys.exc_info()[0]))
-        print("Error while reading config file: %s" % sys.exc_info()[0])
-        sys.exit(-1)
+    except Exception:
+        LOGGER.error("##### ERROR Failed to read config file ./conf/%s [ due to: %s" % (file_name, sys.exc_info()[0]))
+        log_exit()
+
 
 # read from configuration file a base_path with videos linked to pins
-BASE_PATH = read_base_path_config("./config").strip()
+BASE_PATH = read_base_path_config("conf/base_path").strip()
 
-def show_pins() :
+
+def show_pins():
     """
     List all subfolders which matchind pin configuration
     :return: list of subfolder with numeric name only
     """
-    return [folder for folder in os.listdir(BASE_PATH) if R.match(folder)]
+    try:
+        directories = os.listdir(BASE_PATH)
+        return [f for f in directories if R.match(f)]
+    except Exception:
+        LOGGER.error("##### ERROR Failed to read pin directories path [%s] due to: %s" % (BASE_PATH, sys.exc_info()[0]))
+        log_exit()
 
-def play_video(pin) :
-    Popen(['omxplayer', '-b', video_path(pin)])
 
-def video_path(pin) :
+def extract_reset_pin():
+    return read_base_path_config('conf/reset').strip()
+
+
+def play_video(pin):
+    stop_video()
+    video = video_path(pin)
+    LOGGER.info("starting playback y pin = %s -> video = %s" % (pin,video))
+    try:
+        Popen(['omxplayer', '-b', '-o', 'hdmi', video, ' > /dev/null']).wait()
+    except Exception:
+        LOGGER.error("##### ERROR while playback of video due to : %s" % sys.exc_info()[0])
+        pass
+    loop_video()
+
+
+def loop_video():
+    stop_video()
+    try:
+        Popen(['omxplayer', '-b', '-o', 'hdmi', '--loop', video_path('default'), ' > dev/null'])
+    except Exception:
+        LOGGER.error("##### ERROR video playback due to %s" % sys.exc_info()[0])
+        pass
+
+
+def stop_video():
+    try:
+        Popen(['killall', 'omxplayer.bin']).wait()
+    except Exception:
+        LOGGER.error("##### ERROR while interrupting video playback")
+        pass
+
+
+def video_path(pin):
     path = "%s/%s/" % (BASE_PATH, pin)
-    first_file = next(os.path.join(path, f) for f in os.listdir(path) if os.path.isfile(os.path.join(path, f)))
-    return first_file
+    try:
+        return next(os.path.join(path, f) for f in os.listdir(path) if os.path.isfile(os.path.join(path, f)))
+    except Exception:
+        LOGGER.error("##### ERROR loading any video file by path: %s" % path)
+        pass
+        return "/dev/null"
 
-def loop() :
+
+def is_pin_triggered(val):
+    return val == 0
+
+
+def loop():
+    loop_video()
     while True:
-        print("Running main loop")
-        logger.debug("Running main loop")
-        time.sleep(0.2)
-        for pin in pin_info.values() :
-            readVal = GPIO.input(pin)
-            print("state of pin[%s] is %s" % (pin, readVal))
-            if readVal == 0 :
+        time.sleep(0.1)
+        for pin in pin_info.values():
+            read_val_pin = GPIO.input(pin)
+            if is_pin_triggered(read_val_pin):
+                LOGGER.info("Pin %s triggered" % pin)
                 play_video(pin)
 
-def main() :
+
+def reset_monitor():
+    reset_pin = int(extract_reset_pin())
+    while True:
+        time.sleep(0.1)
+        read_val = GPIO.input(reset_pin)
+        if is_pin_triggered(read_val):
+            LOGGER.info("RESETTING ALL VIDEOS")
+            loop_video()
+
+
+def main():
     """
     Main service entry-point.
     performs all intialializations and running main loop
     :return: nothing
     """
-    logger.info("starting pi-test....")
-    print("starting pi-test....")
-    for p in show_pins() :
-        pin_info.update({"%s" % p :  int(p)})
-    logger.info("pins are configured: %s" % pin_info.values())
-    print("pins are configured: %s" % pin_info.values())
-    print(("Starting service in loop..."))
-    GPIO.setmode(GPIO.BCM)
-    for p in pin_info.values() :
-        logger.debug("Initializing pin [%s,\tIN,\tGPIO_PUD_UP,\tvideo=%s]" % (p, video_path(p) ))
-        GPIO.setup(p, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    loop()
+    LOGGER.info("\n#############################################")
+    LOGGER.info("starting pi-test....")
 
-main()
+    for p in show_pins():
+        pin_info.update({"%s" % p :  int(p)})
+
+    LOGGER.info("pins are configured: %s" % pin_info.values())
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(int(extract_reset_pin()), GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+    for p in pin_info.values():
+        LOGGER.info("Initializing pin [%s,\tIN,\tGPIO_PUD_UP,\tvideo=%s]" % (p, video_path(p)))
+        GPIO.setup(p, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+    threading.Thread(target=loop).start()
+    threading.Thread(target=reset_monitor).start()
+
+
+if __name__ == '__main__':
+    main()
 
